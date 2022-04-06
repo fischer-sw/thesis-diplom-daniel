a817fabd9fe220542d78f056436174a03df4ecc0
Daniel Fischer <daniel.fischer3@mailbox.tu-dresden.de>
2022-04-06 15:01:26 +0200

    changed python script

--- Software/python/test.py
+++ Software/python/test.py
@@ -1,5 +1,8 @@
 import math
+from tkinter import image_names
 import numpy as np
+import os
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -43,13 +46,13 @@ class flowfield:
             error = True
             print("Parameters in pars {} dict missing").format(missing)
 
-        if pars["r_res"]["length"]%pars["r_res"]["devisions"] != 0:
-            error = True
-            print("Radius length and devisions don't match")
+        # if pars["r_res"]["length"]%pars["r_res"]["devisions"] != 0:
+        #     error = True
+        #     print("Radius length and devisions don't match")
 
-        if pars["z_res"]["length"]%pars["z_res"]["devisions"] != 0:
-            error = True
-            print("Height length and devisions don't match")
+        # if pars["z_res"]["length"]%pars["z_res"]["devisions"] != 0:
+        #     error = True
+        #     print("Height length and devisions don't match")
 
         if error == False:    
             return pars
@@ -67,23 +70,32 @@ class flowfield:
 
         field = []
 
-        v_field = np.zeros((r_elements, z_elements))
+        v_field = np.zeros((z_elements+1, r_elements))
 
-        for i in range(1, z_elements):
+        for i in range(z_elements+1):
 
             line = []
 
             for j in range(1, r_elements):
+
+                r_len = self.pars["r_res"]["length"]
+                z_len = self.pars["z_res"]["length"]
                 
-                r = j * int(self.pars["r_res"]["length"]/self.pars["r_res"]["devisions"])
-                z_val = i * int(self.pars["z_res"]["length"]/self.pars["z_res"]["devisions"])
+                r_divs = self.pars["r_res"]["devisions"]
+                z_divs = self.pars["z_res"]["devisions"]
+                
+                r_div_len = r_len/r_divs
+                z_div_len = z_len/z_divs
+
+                r = j * r_div_len
+                z_val = i * z_div_len
                 z = z_val - self.pars["z_res"]["length"]/2
 
                 v_r = self.calc_v(r, z, self.pars)
 
                 tmp = {
-                    "r" : j,
-                    "z" : i,
+                    "r" : r,
+                    "z" : z,
                     "v" : v_r
                 }
 
@@ -93,6 +105,7 @@ class flowfield:
             
             field.append(line)
 
+        # v_field = np.delete(v_field, [0] , 0)
 
         return field, v_field
 
@@ -100,21 +113,74 @@ class flowfield:
         """
         Function that shows calculation results as image
         """
-        plt.matshow(self.flowfield)
 
-        plt.show()
+        data = self.flowfield
+        # data = np.rot90(np.fliplr(data))
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(data, interpolation='nearest', cmap=plt.cm.get_cmap('jet', 10))
         
 
+        # set correct x_ticks labels
+        x_ticks = ax.get_xticks()
+        y_ticks = ax.get_yticks()
+
+        test = ax.get_xticklabels()
+        test1 = ax.get_yticks()
+
+        x_div_len = self.pars["r_res"]["length"]/len(x_ticks)
+
+        new_x_ticks = []
+
+        for x in range(len(x_ticks)):
+            new_x_ticks.append(x * x_div_len)
+        
+        # ax.set_xticklabels(new_x_ticks)
+
+        y_ticks = ax.get_yticks()
+
+        z_div_len = self.pars["z_res"]["length"]/len(y_ticks)
+
+        new_y_ticks = []
+
+        for z in range(len(y_ticks)):
+            new_y_ticks.append(round(self.pars["z_res"]["length"]/2 - z * z_div_len, 2))
+
+        # ax.set_yticklabels(new_y_ticks)
+
+        # add axis description
+        ax.set_xlabel("radius r [m]")
+        ax.set_ylabel("height z [m]")
+
+        # add colorbar
+
+        
+        cbar = fig.colorbar(cax)
+        cbar.set_label('velocity [m/s]', rotation=90)
+
+        path = sys.path[0]
+
+        path = os.path.join(path, "Images")
+
+        if os.path.exists(path) == False:
+            os.mkdir(path)
+
+        image_name = "Test.png"
+
+        image_path = os.path.join(path, image_name)
+
+        plt.savefig(image_path)
 
     def calc_v(self, r, z, pars):
         """
         Function that calculates velocity v dependent on positon (r = radius, z = height, pars = parameters)
         """
         
-        Q = pars["flowrate"]
+        Q_rate = pars["flowrate"]
         h = pars["z_res"]["length"]
 
-        v_max = (3*Q)/(4*math.pi*h*r)
+        v_max = (3*Q_rate)/(4*math.pi*h*r)
 
         return v_max * (1 - ((4 * z**2)/(h**2)))
 
@@ -123,18 +189,21 @@ if __name__ == "__main__":
 
     example = {
         
-        "flowrate" : 3,
+        "flowrate" : 10,
         
         "z_res" : {
-            "length" : 100,
-            "devisions" : 10
+            "length" : 1,
+            "devisions" : 60
         },
 
         "r_res" : {
-            "length" : 50,
-            "devisions" : 25
+            "length" : 5,
+            "devisions" : 80
         }
     }
 
     test = flowfield(example)
     test.show_field()
+    #shape = np.shape(test.flowfield)
+    #print(test.flowfield[round(shape[1]/2)])
+
